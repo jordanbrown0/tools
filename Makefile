9f0fd1c569b8cb8f8e0c56c1946451f975267d49
Jordan Brown <github@jordan.maileater.net>
2022-11-03 23:25:22 -0700

    more resync with desktop

--- Makefile
+++ Makefile
@@ -5,10 +5,16 @@ install:	\
 	$(BIN)			\
 	$(BIN)/dt		\
 	$(BIN)/ghclone		\
+	$(BIN)/git_changed_files    \
+	$(BIN)/includes.py      \
 	$(BIN)/osc.cmk		\
 	$(BIN)/osc.mk		\
 	$(BIN)/osc.tests	\
 	$(BIN)/osclone		\
+	$(BIN)/pngcmp           \
+	$(BIN)/pull             \
+	$(BIN)/rcolyer-git-pull-request \
+	$(BIN)/treegrep         \
 	$(HOME)/.bash_logout	\
 	$(HOME)/.bash_profile	\
 	$(HOME)/.bashrc		\
@@ -32,6 +38,18 @@ $(BIN)/osc.mk:		osc.mk
 	$(INS)
 $(BIN)/osc.tests:	osc.tests
 	$(INS)
+$(BIN)/git_changed_files:	git_changed_files
+	$(INS)
+$(BIN)/includes.py:	includes.py
+	$(INS)
+$(BIN)/pngcmp:	pngcmp
+	$(INS)
+$(BIN)/pull:	pull
+	$(INS)
+$(BIN)/rcolyer-git-pull-request:	rcolyer-git-pull-request
+	$(INS)
+$(BIN)/treegrep:	treegrep
+	$(INS)
 $(HOME)/.bash_logout:	bash_logout
 	$(INS)
 $(HOME)/.bash_profile:	bash_profile
@@ -46,5 +64,4 @@ $(HOME)/.profile:	profile
 	$(INS)
 $(HOME)/.vimrc:		vimrc
 	$(INS)
-
 
---
+++ includes.py
@@ -0,0 +1,54 @@
+import sys
+import re
+import os
+import argparse
+
+include_re = re.compile(r'^#include\s*["<](.*\.h)[">]')
+indent_step = '  '
+
+myname = sys.argv[0]
+del sys.argv[0]
+
+parser = argparse.ArgumentParser(prog='includes.py')
+parser.add_argument('-I', action='append', metavar='dir', dest='includepath')
+parser.add_argument('-f', '--find', action='store', metavar='filename', dest='find')
+parser.add_argument('filename')
+args = parser.parse_args(sys.argv)
+
+def find(dir, name): 
+    p = os.path.join(dir, name)
+    if os.path.exists(p):
+        return p
+    if args.includepath:
+        for idir in args.includepath:
+            p = os.path.join(idir, name)
+            if os.path.exists(p):
+                return p
+    return None
+
+def includes(filename, indent, parents):
+    if filename in parents:
+        return
+
+    dir = os.path.dirname(filename)
+    base = os.path.basename(filename)
+
+    if args.find:
+        if base == args.find:
+            print(' > '.join(parents))
+    else:
+        print(indent+filename)
+    with open(filename) as f:
+        for line in f:
+            m = include_re.match(line)
+            if m:
+                included = m.group(1)
+                fullpath = find(dir, included)
+                if not fullpath:
+                    if not args.find:
+                        print(indent + indent_step + included + ' (not found)')
+                else:
+                    includes(fullpath, indent + indent_step, parents + [base])
+
+
+includes(args.filename, "", []);
